054a11024635c931cd8835a34103e1532b65c768
Johan Tan <johan.tan21@gmail.com>
2018-10-25 16:05:54 -0700

    Fixed all functionality from non ShardedBot to work on the AutoShardedBot Fixed gitignore to ignore a key text file

--- cogs/MCBot.py
+++ cogs/MCBot.py
@@ -1,11 +1,16 @@
 # Author: Johan Tan
 import sys
+import time
 try:
     import discord
     from discord.ext import commands
 except ImportError:
     print("Discord.py is not installed.\n")
     sys.exit(1)
+try:
+    from trello import TrelloClient
+except ImportError:
+    print("Py-trello is not installed.\n")
 from utils import permissions, default
 class MCBot:
     def __init__(self, bot):
@@ -15,7 +20,7 @@ class MCBot:
         self.discord_tk = config.discord_token
         self.trello_ky = config.trello_key
         self.trello_tk = ''
-        self.bot_color = config.bot_color
+        self.bot_color = 0xf0efc8
         self.channel = ''
         self.join_message = config.join_message
         self.current_game = config.playing
@@ -25,25 +30,25 @@ class MCBot:
         """Assigns bot to the current channel
         the command is on."""
         self.channel = ctx.message.channel
-        await self.send_message(self, ctx, "Assigned to " + str(ctx.message.channel.name))
+        await self.send_message(ctx, "Assigned to " + str(ctx.message.channel.name))
 
     @commands.command()
     async def pingtrello(self, ctx):
         """Checks bot's connectivity to Trello"""
-        if(check_trello()):
+        if(self.check_trello()):
             start = time.time()
             boards = self.trello_client.list_boards()
             end = time.time()
-            embed = discord.Embed(title="Pong!", description="Time elapsed {time:3f} seconds".format(time=(end-start)), color=self.bot_color)
-            await self.send_embed(self, ctx, embed)
+            embed = discord.Embed(title="Pong!", description="Time elapsed {time:3f} seconds".format(time=(end-start)), color=int(self.bot_color))
+            await self.send_embed(ctx, embed)
         else:
-            embed = discord.Embed(title="Error!", description="Trello client not connected. Please use MC!token and MC!link to connect the trello to the bot.", color=self.bot_color)
-            await self.send_embed(self, ctx, embed)
+            embed = discord.Embed(title="Error!", description="Trello client not connected. Please use MC!token and MC!link to connect the trello to the bot.", color=int(self.bot_color))
+            await self.send_embed(ctx, embed)
     @commands.command()
     async def token(self, ctx):
         """Supplies the user a link to determine
         the token that the bot will use."""
-        await send_message(self, ctx, "https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Moon%20CommandBot&key="+self.trello_ky)
+        await self.send_message(ctx, "https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=Moon%20CommandBot&key="+self.trello_ky)
     @commands.command()
     async def link(self, ctx, api_token):
         """Links the bot to the trello account
@@ -53,8 +58,8 @@ class MCBot:
             api_key=self.trello_ky,
             token=self.trello_tk
         )
-        await send_message(self, ctx, "Received token of Token: " + self.trello_tk)
-    def check_trello():
+        await self.send_message(ctx, "Received token of Token: " + self.trello_tk)
+    def check_trello(self):
         if(self.trello_client == ""):
             return False
         else:
